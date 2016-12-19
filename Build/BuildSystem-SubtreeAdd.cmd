5f134b90b1d8f43de16d705d87821f01af0b4ee4
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2016-12-19 14:32:54 +1100

    Added build scripts Task 31534:

---
+++ Build/BuildSystem-SubtreeAdd.cmd
@@ -0,0 +1,7 @@
+Rem from https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree
+Rem You should create aliases sba and sbu as described in https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree
+Rem If you have errors "Working tree has modifications.  Cannot add." run stash before commang and stash apply after command
+Rem git stash 
+git sba https://github.com/Webjet/PsakeBuildScript.git Build/BuildSystem
+rem git stash apply
+@pause
