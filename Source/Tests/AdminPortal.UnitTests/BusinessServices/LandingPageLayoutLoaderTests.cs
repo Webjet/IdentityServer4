c4edfed51ede1c4cfdb602877a39c479982a9d41
kajal bhatia <kajal.bhatia@webjet.com.au>
2016-12-23 16:43:27 +1100

    Upgrading to v4.6.1

---
+++ Source/Tests/AdminPortal.UnitTests/BusinessServices/LandingPageLayoutLoaderTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdminPortal.BusinessServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPortal.BusinessServices.Tests
+{
+    [TestClass(),Ignore]
+    public class LandingPageLayoutLoaderTests
+    {
+        [TestMethod()]
+        public void LandingPageLayoutLoaderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LandingPageLayoutLoaderTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetConfigurationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Source/Tests/AdminPortal.UnitTests/BusinessServices/ResourceToApplicationRolesMapperTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdminPortal.BusinessServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPortal.BusinessServices.Tests
+{
+    [TestClass(),Ignore]
+    public class ResourceToApplicationRolesMapperTests
+    {
+        [TestMethod()]
+        public void ResourceToApplicationRolesMapperTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void GetAllowedRolesForResourceTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void IsUserRoleAllowedForResourceTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
