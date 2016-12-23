f411b8dd67a4eaa01dd587eea34deffead8a3dbb
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2016-12-23 12:58:51 +1100

    Build changes

--- Deployment/DODO-SubtreePull.cmd
+++ Deployment/DODO-SubtreePull.cmd
@@ -1,5 +1,4 @@
 Rem from https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree create aliases in ~/.gitconfig
-Rem You should create aliases sba and sbu as described in https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree
 Rem If you have errors "Working tree has modifications.  Cannot add." run stash before commang and stash apply after command
 Rem  git stash 
 @Rem git sbu https://github.com/Webjet/DODO.git Deployment/DODO
