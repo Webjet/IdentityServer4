9dcfde2c88b36c40f400b8e54a9d01d81b060834
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2016-12-28 12:36:11 +1100

    Build/BuildScripts

--- Build/BuildScripts/SubTreeCommands/BuildScripts-Subtree-Push.cmd
+++ Build/BuildScripts/SubTreeCommands/BuildScripts-Subtree-Push.cmd
@@ -2,5 +2,5 @@ Rem from https://medium.com/@v/git-subtrees-a-tutorial-6ff568381844#.a2ne9vlve
 Rem git subtree push
 #run this command from the toplevel of the working tree
 cd ..\..\.. 
-git subtree push --prefix=Build/BuildScripts https://github.com/Webjet/PsakeBuildScript.git master
+git subtree push --prefix=Build/BuildScripts https://github.com/Webjet/BuildScripts.git master
 @pause
