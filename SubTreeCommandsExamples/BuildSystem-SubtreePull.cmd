41e2eb89d0cc0c79aca52a85b388d154055aa149
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2016-12-28 11:02:44 +1100

    Squashed 'Build/BuildScripts/' content from commit 70010d4
    git-subtree-dir: Build/BuildScripts
    git-subtree-split: 70010d47644f56c8b09ee3fa0c08ef3a925f646d

---
+++ SubTreeCommandsExamples/BuildSystem-SubtreePull.cmd
@@ -0,0 +1,7 @@
+Rem from https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree create aliases in ~/.gitconfig
+Rem You should create aliases sba and sbu as described in https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree
+Rem If you have errors "Working tree has modifications.  Cannot add." run stash before commang and stash apply after command
+Rem  git stash 
+git subtree pull --prefix Build/BuildSystem https://github.com/Webjet/BuildScripts.git master --squash
+Rem git stash apply
+@pause
