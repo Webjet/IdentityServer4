4c47ca76983afa361b25fb6021adbfd690129085
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2016-12-28 11:36:36 +1100

    Update to BuildScripts

--- Build/BuildScripts/SubTreeCommandsExamples/BuildSystem-SubtreePull.cmd
+++ Build/BuildScripts/SubTreeCommandsExamples/BuildSystem-SubtreePull.cmd
@@ -2,6 +2,8 @@ Rem from https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree c
 Rem You should create aliases sba and sbu as described in https://developer.atlassian.com/blog/2015/05/the-power-of-git-subtree
 Rem If you have errors "Working tree has modifications.  Cannot add." run stash before commang and stash apply after command
 Rem  git stash 
-git subtree pull --prefix Build/BuildSystem https://github.com/Webjet/BuildScripts.git master --squash
+@rem run this command from the toplevel of the working tree
+cd ..\..\..
+git subtree pull --prefix Build/BuildScripts https://github.com/Webjet/BuildScripts.git master --squash
 Rem git stash apply
 @pause
