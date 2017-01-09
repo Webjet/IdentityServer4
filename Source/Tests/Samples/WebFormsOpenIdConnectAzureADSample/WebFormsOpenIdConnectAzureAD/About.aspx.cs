9acc2193d87a6fa778b6bcb822e42c9afb56f49e
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2017-01-09 12:22:07 +1100

    Source\Tests\Samples\WebFormsOpenIdConnectAzureADSample

---
+++ Source/Tests/Samples/WebFormsOpenIdConnectAzureADSample/WebFormsOpenIdConnectAzureAD/About.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsOpenIdConnectAzureAD
+{
+    [PrincipalPermission(SecurityAction.Demand, Role = "Admin")]
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
