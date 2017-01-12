6a4e10bb614be50cb7841768d5dcfa974483b55f
mfreidgeim <Michael.Freidgeim@webjet.com.au>
2017-01-12 17:54:36 +1100

    the address used for sign in and sign out is picked up dynamically from the request Task 31760:Dynamically assign redirect from AAD URI in the sign in message

--- Source/AdminPortal/App_Start/Startup.Auth.cs
+++ Source/AdminPortal/App_Start/Startup.Auth.cs
@@ -4,21 +4,12 @@ using System.Configuration;
 using System.Globalization;
 using System.IdentityModel.Claims;
 using System.Linq;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Globalization;
-using System.IdentityModel.Claims;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Microsoft.IdentityModel.Protocols;
-using Microsoft.Owin.Extensions;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Notifications;
 using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Owin;
 using AdminPortal.Models;
 
@@ -30,7 +21,7 @@ namespace AdminPortal
         private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
         private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
         private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
-        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        //  use RedirectToIdentityProvider instead of private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
 
         public static readonly string Authority = aadInstance + tenantId;
 
@@ -42,13 +33,13 @@ namespace AdminPortal
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
-           Uri test= HttpContext.Current.Request.Url;
+
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
                     ClientId = clientId,
                     Authority = Authority,
-                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    //  use RedirectToIdentityProvider instead of PostLogoutRedirectUri = postLogoutRedirectUri,
 
                     //Required for AAD Role Based Authentication
                     TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
@@ -59,8 +50,18 @@ namespace AdminPortal
 
                     Notifications = new OpenIdConnectAuthenticationNotifications()
                     {
+                        RedirectToIdentityProvider = (context) =>
+                        {  //from https://github.com/Azure-Samples/active-directory-dotnet-webapp-multitenant-openidconnect/blob/master/TodoListWebApp/App_Start/Startup.Auth.cs
+                            // This ensures that the address used for sign in and sign out is picked up dynamically from the request
+                            // this allows you to deploy your app (to Azure Web Sites, for example)without having to change settings
+                            // Remember that the base URL of the address used here must be provisioned in Azure AD beforehand.
+                            string appBaseUrl = context.Request.Scheme + "://" + context.Request.Host + context.Request.PathBase;
+                            context.ProtocolMessage.RedirectUri = appBaseUrl;
+                            context.ProtocolMessage.PostLogoutRedirectUri = appBaseUrl;
+                            return Task.FromResult(0);
+                        },
                         // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
-                       AuthorizationCodeReceived = (context) => 
+                        AuthorizationCodeReceived = (context) => 
                        {
                            var code = context.Code;
                            ClientCredential credential = new ClientCredential(clientId, appKey);
