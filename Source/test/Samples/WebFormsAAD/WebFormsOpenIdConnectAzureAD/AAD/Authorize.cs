c46bf96fb14ba7684f6296d6534d2b1782994fa0
kajal bhatia <kajal.bhatia@webjet.com.au>
2017-02-16 13:03:20 +1100

    Adding missing files from  test\sample\WebForm

--- Source/test/Samples/WebFormsAAD/WebFormsOpenIdConnectAzureAD/AAD/Authorize.cs
+++ Source/test/Samples/WebFormsAAD/WebFormsOpenIdConnectAzureAD/AAD/Authorize.cs
@@ -23,8 +23,6 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
 using Newtonsoft.Json;
-using WebFormsOpenIdConnectAzureAD.AAD;
-
 //using TSA.Applications.WebjetTsa;
 //using TSA.BusinessEntities;
 
@@ -32,34 +30,107 @@ namespace WebFormsOpenIdConnectAzureAD
 {
     public class Authorize : CodeAccessSecurityAttribute
     {
-          public Authorize(SecurityAction action) : base(action)
+        private string adminPortalApiResourceId = ConfigurationManager.AppSettings["AdminPortalApi:ResourceId"];
+        private string adminPortalApiBaseAddress = ConfigurationManager.AppSettings["AdminPortalApi:BaseAddress"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string _clientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string _brandRegion = ConfigurationManager.AppSettings["BrandRegionSuffix"];//ToDO change new SiteInfo().BrandRegion;//
+        private bool _adminPortalApiMock = true;
+        private HttpContext _httpContext = null;
+        public Authorize(SecurityAction action) : base(action)
         {
             Debug.Assert(HttpContext.Current!=null);
-  //        _httpContext = HttpContext.Current;
+          _httpContext = HttpContext.Current;
             Global.LogSession(MethodBase.GetCurrentMethod().Name, this);
-            ThrowExceptionIfNoPermission = true;
         }
 
-        public bool ThrowExceptionIfNoPermission { get; set; }
-
-        public string ResourceKey { get; set; }
+       public string ResourceKey { get; set; }
 
         public override IPermission CreatePermission()
         {
-            var resourcePermissionsChecker = new ResourcePermissionsChecker();
-            bool isAllowed = resourcePermissionsChecker.IsResourceAllowed(ResourceKey);
-            //string resourceKey = ResourceKey + "_" + _brandRegion;
-            //Task<bool> task = Task.Run<bool>(async () => await GetIsResourceAllowedAsync(_httpContext, resourceKey));
-            //bool isAllowed = task.Result;
+            string resourceKey = ResourceKey + "_" + _brandRegion;
+            Task<bool> task = Task.Run<bool>(async () => await GetHttpResponseFromAdminPortalApiAsync(_httpContext, resourceKey));
+            bool isAllowed = task.Result;
             var permissionState = isAllowed ? PermissionState.Unrestricted : PermissionState.None;
-            if (isAllowed == false && ThrowExceptionIfNoPermission)
-            {
-                resourcePermissionsChecker.ThrowException(ResourceKey);
-            }
             return new PrincipalPermission(permissionState);
+           //     throw new SecurityException("Sorry!! You do not have access permission for requested resource " + resourceKey);
+
+
         }
 
-       
-      
+        private async Task<bool> GetHttpResponseFromAdminPortalApiAsync(HttpContext httpContext, string resourceKey)
+        {
+            string accessToken = null;
+            bool userAllowedForResource = false;
+            try
+            {
+                string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            AuthenticationContext authContext = Startup.NewAuthenticationContext(userObjectID, httpContext); //new AuthenticationContext(Startup.Authority, new ADALTokenCache(userObjectID));
+            Debug.Assert(authContext.TokenCache.Count > 0);
+            ClientCredential credential = new ClientCredential(clientId, _clientSecret);
+
+                AuthenticationResult result = await authContext.AcquireTokenSilentAsync(adminPortalApiResourceId, credential, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId)).ConfigureAwait(false);
+
+                accessToken = result.AccessToken;
+            }
+            catch (AdalSilentTokenAcquisitionException ex)
+            {
+                //http://stackoverflow.com/questions/41519132/how-to-store-the-token-received-in-acquiretokenasync-with-active-directory
+                //https://docs.microsoft.com/en-us/aspnet/core/security/authentication/cookie
+                //try to get token as in start 
+
+                //   Debug.WriteLine(ex.ToString());
+                throw;
+            }
+
+            //try
+            //{
+            //// API in ASP .Net MVC, named as 'WebejetAdminPortal', coupled with AdminPortal solution
+            //string adminPortalApiSuffix = "/api/AllowedRolesForResource/ReviewPendingBookings_WebjetAU";
+            //string adminPortalApiSuffix = "/api/AllowedRolesForResource"; 
+
+            //// API in ASP Core named as 'AdminPortalWebApi' and added as separate project in AdminPortal solution
+            string adminPortalApiSuffix = "/api/AllowedRolesForResource/" + resourceKey; //"/api/values"; 
+                HttpResponseMessage response = null;
+                if (!adminPortalApiBaseAddress.StartsWith("Mock:"))
+                {
+                    HttpClient client = new HttpClient();
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, adminPortalApiBaseAddress + adminPortalApiSuffix);
+
+                    //http://brainof-dave.blogspot.com.au/2008/08/remote-certificate-is-invalid-according.html
+                    ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                    { return true; };
+
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                    response = await client.SendAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    userAllowedForResource = responseString == "True" ? true : false;
+                    }
+                    else
+                    {
+                       Debug.WriteLine("request:"+request.ToString() + "response:" + response.ToString());//TODO: Logger.
+                    }
+                }
+                else
+                {
+                    response = new HttpResponseMessage() { StatusCode = HttpStatusCode.OK, Content = new StringContent("") };
+                    userAllowedForResource = true;
+                }
+            //}
+
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.ToString());
+            //}
+
+
+            return userAllowedForResource;
+        }
+
     }
 }
---
+++ Source/test/Samples/WebFormsAAD/WebFormsOpenIdConnectAzureAD/Global.asax.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using NLog;
+
+namespace WebFormsOpenIdConnectAzureAD
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+           
+
+        }
+        void Session_Start(object sender, EventArgs e)
+        {
+            LogSession(MethodBase.GetCurrentMethod().Name, sender);
+            Session["RequestCounter"] = 0;
+            LogSession(MethodBase.GetCurrentMethod().Name + " after assignment " , sender);
+        }
+
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+  //           LogSession(MethodBase.GetCurrentMethod().Name, sender);
+
+        }
+
+        protected void Application_EndRequest(Object sender, EventArgs e)
+        {
+          //  LogSession(MethodBase.GetCurrentMethod().Name, sender);
+
+        }
+
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            // Logger.Debug(" FormsAuthenticationCookie    {0} \n FormsAuthentication={1} Request.IsAuthenticated={2} ", TraceOutputHelper.OutputFormsAuthenticationCookie(Request), TraceOutputHelper.FormsAuthenticationAsString("Application_AuthenticateRequest"), Request.IsAuthenticated);
+           // LogSession(MethodBase.GetCurrentMethod().Name, sender);
+        }
+        protected void Application_PreRequestHandlerExecute(Object sender, EventArgs e)
+        {
+            LogSession(MethodBase.GetCurrentMethod().Name, sender);
+
+        }
+        protected void Application_PostRequestHandlerExecute(Object sender, EventArgs e)
+        {
+            LogSession(MethodBase.GetCurrentMethod().Name, sender);
+
+        }
+
+        public static void LogSession(string methodName, object sender)
+        {
+            Debug.WriteLine("LogSession:" + methodName + " sender " + sender + " Sessions Count " + HttpContext.Current?.Session?.Count);
+        }
+    }
+}
