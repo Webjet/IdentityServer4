0ff665cb0d9fc383be8defc959008e5fa524820e
kajal bhatia <kajal.bhatia@webjet.com.au>
2017-02-17 15:13:16 +1100

    Adding StatusCode middleware after OpenIdConnect authentication to avoid redirect to Erroe\401.html , statuscode 401 for every authenticated and authorised users

--- Source/src/AdminPortal/Controllers/ErrorController.cs
+++ Source/src/AdminPortal/Controllers/ErrorController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Dispatcher;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,10 +26,16 @@ namespace AdminPortal.Controllers
         [HttpGet("/Error/{statusCode}")]
         public IActionResult Index(int statusCode)
         {
-            int httpStatusCode=HttpContext.Response.StatusCode;
-            var statusCodePageFeature = HttpContext.Features.Get<IStatusCodePagesFeature>();
-            
-            return View(viewName: statusCode.ToString());
+            var httpResponseStatusCode = HttpContext.Response.StatusCode;
+           // if (httpResponseStatusCode != 200)
+           // {
+                var statusCodePageFeature = HttpContext.Features.Get<IStatusCodePagesFeature>();
+                var feature = HttpContext.Features.Get<IHttpRequestFeature>();
+                var error = HttpContext.Features.Get<IErrorHandler>();
+                return View(viewName: statusCode.ToString());
+           // }
+
+           // return RedirectToAction("Index", "Home");
         }
     }
 }
