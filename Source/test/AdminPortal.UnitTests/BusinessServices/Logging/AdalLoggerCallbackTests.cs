4291c708bfbc16077987848fcfe93120f41be83c
michael-freidgeim-webjet <michael.freidgeim@webjet.com.au>
2017-02-18 15:22:55 +1100

    added AdalLoggerCallbackTests.cs

---
+++ Source/test/AdminPortal.UnitTests/BusinessServices/Logging/AdalLoggerCallbackTests.cs
@@ -0,0 +1,26 @@
+﻿using AdminPortal.BusinessServices.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace AdminPortal.UnitTests.BusinessServices.Logging
+{
+    [TestClass()]
+    public class AdalLoggerCallbackTests
+    {
+       
+        [TestMethod()]
+        public void LogTest()
+        {
+          //  var memoryTarget = NlogTestsHelper.CreateMemoryTarget();
+            ILogger logger= Substitute.For<ILogger>();
+            new AdalLoggerCallback(logger).Log(Microsoft.IdentityModel.Clients.ActiveDirectory.LogLevel.Information,"just test" );
+            logger.Received(1).LogTrace(Arg.Is<string>(x => x.Contains("just test")));// Arg.Any<string>());
+            logger.Received(1).LogTrace(Arg.Is<string>(x => x.Contains("Information"))); 
+            //logEntry.Should().Contain("just test");
+            //logEntry.Should().Contain("Information");
+
+        }
+
+    }
+}
