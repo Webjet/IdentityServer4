d79190fbefe89bfbd1967197aeb2899e5b116973
michael-freidgeim-webjet <michael.freidgeim@webjet.com.au>
2017-02-28 17:49:03 +1100

    Squashed 'Build/BuildScripts/' changes from 7f26de50..ee40823b
    ee40823b PS Get-FunctionName
    5a131422 if (Test-Path "$PSScriptRoot\DODO\Local" )
    cf73c6c7 MS Deploy added to project
    0975c37a CoveragePercentUpdate Debug=false
    fc506a1e Build/BuildScripts
    8590ea30 run this command from the toplevel of the working tree cd ..\..\..
    56bda1bf run this command from the toplevel of the working tree cd ../../..
    f8534f35 Merge commit 'a6e82ff7360883f26a89a2cebab2a0cb23694de2'
    5e7a8728 Merge commit '637024991c12857bb3dd10b689465b33dd9d1acb'
    f483c5e8 Update to BuildScripts
    
    git-subtree-dir: Build/BuildScripts
    git-subtree-split: ee40823b18bae8d3102a38885e6aa57d8fd7cd91

--- SubTreeCommands/BuildScripts-Subtree-Push.cmd
+++ SubTreeCommands/BuildScripts-Subtree-Push.cmd
@@ -1,6 +1,6 @@
 Rem from https://medium.com/@v/git-subtrees-a-tutorial-6ff568381844#.a2ne9vlve
 Rem git subtree push
-#run this command from the toplevel of the working tree
-cd .. 
-git subtree push --prefix=Build/BuildScripts https://github.com/Webjet/PsakeBuildScript.git master
+@rem run this command from the toplevel of the working tree
+cd ..\..\.. 
+git subtree push --prefix=Build/BuildScripts https://github.com/Webjet/BuildScripts.git master
 @pause
